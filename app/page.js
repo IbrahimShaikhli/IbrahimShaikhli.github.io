64db4c909ffb2d87f2521e784b6bf3e0ca39be91
Ibrahim Khalid <Ibrahim.AlShaikhli@outlook.com>
2023-08-29 13:45:25 +0400

    aaa

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
-import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
+import devEdWaveImage from '../public/dev-ed-wave.png';
 
 export default function Home() {
   return (
@@ -12,15 +13,15 @@ export default function Home() {
       <main className=' bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-          <h1 className='text- xl font-burtons'>DevelopedbyIbrahim</h1>
-          <ul className='flex items-center'>
-            <li>
-              <BsFillMoonStarsFill className=' cursor-pointer text-xl'/>
-            </li>
-            <li>
-              <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
-            </li>
-          </ul>
+            <h1 className='text- xl font-burtons'>DevelopedbyIbrahim</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className=' cursor-pointer text-xl' />
+              </li>
+              <li>
+                <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
+              </li>
+            </ul>
           </nav>
           <div className=' text-center p-10 '>
             <h2 className='text-5xl py-2 text-teal-600 font-medium'>
@@ -32,9 +33,6 @@ export default function Home() {
           <div className='text-5xl flex justify-center gap-10 py-3 text-gray-600'>
             <AiFillLinkedin />
             <AiFillYoutube />
-          </div>
-          <div>
-            
           </div>
         </section>
       </main>
