d615570eef7256b4c0026f58a628897574ca1ff7
Ibrahim Khalid <Ibrahim.AlShaikhli@outlook.com>
2023-09-01 20:01:33 +0400

    aaa

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { FaFileDownload } from "react-icons/fa";
-
 import Image from "next/image";
 import ProjectCard from "./projectCard";
 
 export default function Home() {
-
   const projectData = [
     {
       title: "Souq Artisan",
@@ -29,8 +28,15 @@ export default function Home() {
       thumbnailSrc: "/comingsoon.png",
       githubLink: "",
     },
-
   ];
+
+  // State to control the mobile menu visibility
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <div>
@@ -39,29 +45,69 @@ export default function Home() {
         <link rel="icon" href="/i-logo.png" />
       </head>
 
-      <main className="bg-black text-white px-10">
-
-        <nav className="py-10 mb-12 flex justify-between sticky top-0 z-50">
-          <h1 className="text-3xl font-semibold">Ibrahim</h1>
-          <ul className="flex items-center space-x-6">
+      <main className="bg-black text-white px-4">
+        <nav className="py-4 mb-6 flex justify-between sticky top-0 z-50">
+          <h1 className="text-3xl md:text-4xl font-semibold">Ibrahim</h1>
+          {/* Hamburger icon for mobile */}
+          <div
+            className="md:hidden cursor-pointer text-3xl"
+            onClick={toggleMobileMenu}
+          >
+            &#9776;
+          </div>
+          {/* Mobile menu */}
+          {isMobileMenuOpen && (
+            <div className="md:hidden absolute top-20 left-0 w-full bg-black text-white py-4 text-center">
+              <ul className="space-y-4">
+                <li>
+                  <a
+                    className="cursor-pointer text-xl hover:text-purple-300 hover-glow"
+                    href="#about"
+                    onClick={toggleMobileMenu}
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="cursor-pointer text-xl hover:text-purple-300 hover-glow"
+                    href="#projects"
+                    onClick={toggleMobileMenu}
+                  >
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="cursor-pointer text-xl hover:text-purple-300 hover-glow"
+                    href="#contact"
+                    onClick={toggleMobileMenu}
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+          {/* Desktop navigation */}
+          <ul className="hidden md:flex items-center space-x-6">
             <li>
-              <a className="cursor-pointer text-xl hover:text-purple-300 hover-glow" href="#about">
+              <a className="cursor-pointer text-xl md:text-lg hover:text-purple-300 hover-glow" href="#about">
                 About
               </a>
             </li>
             <li>
-              <a className="cursor-pointer text-xl hover:text-purple-300 hover-glow" href="#projects">
+              <a className="cursor-pointer text-xl md:text-lg hover:text-purple-300 hover-glow" href="#projects">
                 Projects
               </a>
             </li>
             <li>
-              <a className="cursor-pointer text-xl hover:text-purple-300 hover-glow" href="#contact">
+              <a className="cursor-pointer text-xl md:text-lg hover:text-purple-300 hover-glow" href="#contact">
                 Contact
               </a>
             </li>
           </ul>
         </nav>
-
 
         <section id="about" className="min-h-screen py-20 text-white relative">
           <div
